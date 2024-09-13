fbccc0c99b952d02d5eca7bedbdcb6c942f55337
surya-mac <bumantara.surya2213@gmail.com>
2024-09-13 11:10:36 +0700

    init project

---
+++ bambinaut-foundation-ios/bambinaut-foundation-ios/bambinaut_foundation_iosApp.swift
@@ -0,0 +1,17 @@
+//
+//  bambinaut_foundation_iosApp.swift
+//  bambinaut-foundation-ios
+//
+//  Created by MacBook Air on 13/09/24.
+//
+
+import SwiftUI
+
+@main
+struct bambinaut_foundation_iosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
