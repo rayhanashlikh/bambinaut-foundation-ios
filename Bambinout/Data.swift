856d4c852230c05b2560685a27a5ef7a315c6db7
surya-mac <bumantara.surya2213@gmail.com>
2024-09-13 16:37:20 +0700

    add UIColor extension and AppColor variable

--- Bambinout/Data.swift
+++ Bambinout/Data.swift
@@ -17,6 +17,37 @@ struct IngredientData {
     let for_weight_status: Int
 }
 
+struct AppColors {
+    static let lightBlue = "#D1E9F6"
+    static let beige = "#F6EACB"
+    static let peach = "#F1D3CE"
+    static let pink = "#EECAD5"
+}
+
+extension UIColor {
+  convenience init?(hexString hex: String) {
+    var cString: String = hex.trimmingCharacters(in: .whitespacesAndNewlines).uppercased()
+
+    if cString.hasPrefix("#") {
+      cString.remove(at: cString.startIndex)
+    }
+
+    guard cString.count == 6 else {
+      return nil
+    }
+
+    var rgbValue: UInt64 = 0
+    Scanner(string: cString).scanHexInt64(&rgbValue)
+
+    self.init(
+      red: CGFloat((rgbValue & 0xFF0000) >> 16) / 255.0,
+      green: CGFloat((rgbValue & 0x00FF00) >> 8) / 255.0,
+      blue: CGFloat(rgbValue & 0x0000FF) / 255.0,
+      alpha: CGFloat(1.0)
+    )
+  }
+}
+
 
 
 func getDummyIngredients(n: Int) -> [IngredientData] {
