481eea72b32c442c1e3e202c04d704948b92aff5
surya-mac <bumantara.surya2213@gmail.com>
2024-09-18 08:55:11 +0700

    add BabyDataModel observables

--- Bambinout/ForYouView.swift
+++ Bambinout/ForYouView.swift
@@ -8,17 +8,11 @@
 import SwiftUI
 struct ForYouView: View {
     @StateObject private var searchDataModel = SearchDataModel()
-    private var dummyBabyData = BabyDataModel(
-        id: 1,
-        allergy_ids: [1,3],
-        latest_weight: 6,
-        latest_weight_date: getDate(date: "2024-09-13"),
-        birth_date: getDate(date: "2024-02-03")
-    )
+    @StateObject private var dummyBabyData = BabyDataModel(babyData: getDummyBaby())
     var body: some View {
         NavigationStack {
            VStack {
-               IngredientCollections(search: $searchDataModel.searchText)
+               IngredientCollections(search: $searchDataModel.searchText, babyData: $dummyBabyData.babyData)
            }
            .navigationTitle("For You")
         }.searchable(text: $searchDataModel.searchText)
--- Bambinout/Observables.swift
+++ Bambinout/Observables.swift
@@ -6,17 +6,9 @@ class SearchDataModel: ObservableObject {
 }
 
 class BabyDataModel: ObservableObject {
-    @Published var id: Int
-    @Published var allergy_ids: [Int]
-    @Published var latest_weight: Int
-    @Published var latest_weight_date: Date?
-    @Published var birth_date: Date?
+    @Published var babyData: BabyData
     
-    init(id: Int, allergy_ids: [Int], latest_weight: Int, latest_weight_date: Date? = nil, birth_date: Date? = nil) {
-        self.id = id
-        self.allergy_ids = allergy_ids
-        self.latest_weight = latest_weight
-        self.latest_weight_date = latest_weight_date
-        self.birth_date = birth_date
+    init(babyData: BabyData) {
+        self.babyData = babyData
     }
 }
