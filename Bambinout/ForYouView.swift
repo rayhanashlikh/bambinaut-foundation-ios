85e1d4dc52d6257828d9644c07395466dbf1d1ab
surya-mac <bumantara.surya2213@gmail.com>
2024-09-19 11:25:46 +0700

    update tampilan rekomendasi jika baby profile belum diisi

--- Bambinout/ForYouView.swift
+++ Bambinout/ForYouView.swift
@@ -8,11 +8,35 @@
 import SwiftUI
 struct ForYouView: View {
     @StateObject private var searchDataModel = SearchDataModel()
-    @State var dummyBabyData: BabyData = getDummyBaby()
+    @State var babyData: BabyData? = nil
     var body: some View {
         NavigationStack {
            VStack {
-               ForYouCollections(search: $searchDataModel.searchText, babyData: $dummyBabyData)
+               if let unwrappedBabyData = babyData {
+                   ForYouCollections(search: $searchDataModel.searchText, babyData: Binding<BabyData>(
+                       get: { unwrappedBabyData },
+                       set: { babyData = $0 }
+                   ))
+               } else {
+                   VStack {
+                       
+                       NavigationLink(destination: BabyProfileView()) {
+                           HStack {
+                               Text("Go to baby profile")
+                                   .font(.headline)
+                                   .frame(maxWidth: .infinity)
+                                   .padding()
+                                   .background(Color.yellow)
+                                   .cornerRadius(8)
+                           }
+                       }
+                       HStack {
+                           Image(systemName: "info.circle")
+                           Text("Please fill your baby’s profile to get personalized food recommendation for your baby").font(.caption).multilineTextAlignment(.leading)
+                           Spacer()
+                       }.foregroundColor(.gray)
+                   }.padding(.horizontal, 20)
+               }
            }
            .navigationTitle("For You")
         }.searchable(text: $searchDataModel.searchText)
