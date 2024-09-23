7827b72056c1a04956776c0552ffdbdf32069157
surya-mac <bumantara.surya2213@gmail.com>
2024-09-23 12:23:04 +0700

    add modelcontainer

--- Bambinout/BambinoutApp.swift
+++ Bambinout/BambinoutApp.swift
@@ -6,13 +6,17 @@
 //
 
 import SwiftUI
+import SwiftData
 
 @main
 struct BambinoutApp: App {
-    
     var body: some Scene {
         WindowGroup {
             ContentView()
         }
+        .modelContainer(for: [Ingredient.self,
+                              Allergy.self,
+                              Nutrition.self,
+                              Baby.self])
     }
 }
--- Bambinout/ContentView.swift
+++ Bambinout/ContentView.swift
@@ -3,12 +3,18 @@ import SwiftUI
 
 struct ContentView: View {
     
+    @Environment(\.modelContext) private var context
+    
     var body: some View {
         TabBarView()
     }
 }
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
-    }
+//struct ContentView_Previews: PreviewProvider {
+//    static var previews: some View {
+//        ContentView()
+//    }
+//}
+
+#Preview {
+    ContentView()
 }
